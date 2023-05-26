5c32c9290dd4db339c6a3da590ba029bd310d0b7
Ubuntu <ubuntu@ip-172-31-88-97.ec2.internal>
2023-05-26 06:35:16 +0000

    Fri May 26 06:35:16 UTC 2023

---
+++ RemoteWeb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+GIT=git@github.com:tejazelar/tejazelar.github.io.git
+REPO=tejazelar.github.io
+WEBSITE=https://www.tooplate.com/zip-templates/2103_central.zip
+PKG=apt
+FILE=2103_central
+sudo $PKG update
+sudo $PKG install wget git unzip figlet -y
+git clone $GIT
+mkdir -p webfiles
+cd webfiles
+sudo wget $WEBSITE10
+sudo unzip -o $FILE.zip
+sudo rm -rf ~/$REPO/*
+sudo cp -r $FILE/* ~/$REPO/
+cd ..
+sudo rm -rf webfiles10
+cd ~/$REPO
+git add .
+git status
+sleep 2
+git commit -m "$(date)"
+git push -f
+figlet done
